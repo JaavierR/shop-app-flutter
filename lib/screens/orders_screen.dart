72fc67670847b5458ad573a552a7dcf0378831cf
Javier Reyes <javier.reys@gmail.com>
2020-09-28 23:39:17 -0300

    feat: add side drawer to navigate between orders and products

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/orders.dart' show Orders;
+import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -13,6 +16,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemBuilder: (context, index) => OrderItem(orderData.orders[index]),
         itemCount: orderData.orders.length,
