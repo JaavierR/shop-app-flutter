fc408ac329bc1da2b36970a930b389ec67538385
Javier Reyes <javier.reys@gmail.com>
2020-10-09 18:47:58 -0300

    feat: add personalized animations between the screen transitions, also finished the app.

---
+++ lib/helpers/custom_route.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomRoute<T> extends MaterialPageRoute<T> {
+  CustomRoute({WidgetBuilder builder, RouteSettings settings})
+      : super(
+          builder: builder,
+          settings: settings,
+        );
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    if (settings.name == '/') {
+      return child;
+    }
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
+
+class CustomPageTransitionBuilder extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    // With the line comments below, the animation it's still the same.
+    // if (route.settings.name == '/') {
+    //   return child;
+    // }
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
